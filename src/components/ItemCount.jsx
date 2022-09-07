750c582eef64c49c740ec3f1920b4c851aac1548
diegopedetta <pedettadiego@gmail.com>
2022-09-06 21:52:13 -0300

    contador boton y agregar al carrito

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,41 @@
+import React,{useState,useEffect} from 'react'
+
+const ItemCount = ({initial,stock,onAdd}) => {
+  const [count,setCount] = useState(initial);
+
+  const handleAdd = () =>{
+    if(count<stock){
+       setCount(count+1);
+    }else{
+        alert("NO HAY STOCK")
+    }
+     
+  };
+  const handleDecrement = () => {
+      if(count>1){
+          setCount(count-1);
+      }else{
+          alert("NO PUEDE PASAR DEL 1");
+      }
+  };
+
+  const addCart = () =>{
+      onAdd(count);
+      setCount(initial);
+  }
+
+  useEffect(()=>{
+    console.log('xd');
+  },[count]); 
+
+  return (
+    <div>
+        <button onClick={handleDecrement}>-</button> 
+        <h2>{count}</h2>
+        <button onClick={handleAdd}>+</button>
+        <button onClick={addCart}>Agregar al carrito</button>
+    </div>
+  )
+}
+
+export default ItemCount
--- src/container/ItemListContainer.jsx
+++ src/container/ItemListContainer.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import ItemCount from '../components/ItemCount'
 
 export const ItemListContainer = ({greeting}) => {
+
+  const agregarAlCarrito = (cantidad) => {
+    console.log(`Agregado al carrito ${cantidad}`);
+  };
+
   return (
     <div>
       <h1>{greeting}</h1>
+      <ItemCount initial={1} stock={5} onAdd={agregarAlCarrito}/>
     </div>
   )
 }
